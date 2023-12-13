673b7908fc239082ba465ca8278250df10f437f6
viswanath11 <40738371+viswanath11@users.noreply.github.com>
2023-12-13 23:56:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-# Configure Resource Group
-resource "azurerm_resource_group" "example" {
-  name = "example-rg"
-  location = "West Europe"
-}
-
-# Create Storage Account
-resource "azurerm_storage_account" "example" {
-  name = "example-storage"
-  resource_group_name = azurerm_resource_group.example.name
-  location = azurerm_resource_group.example.location
-  sku {
-    name = "Standard_LRS"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.0"
+    }
   }
 }
+
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "East US"
+}
+
